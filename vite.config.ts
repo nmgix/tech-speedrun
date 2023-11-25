4682921b25b00c7453002300ab25f77c43497917
Nmgix <frostdraky@gmail.com>
2023-11-25 09:09:45 +0300

    Добавил minifier и настроил console в prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { terser } from "rollup-plugin-terser";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      minify: "terser",
+      rollupOptions: {
+        plugins: [
+          terser({
+            format: {
+              comments: false
+            },
+            mangle: {
+              keep_classnames: false,
+              reserved: []
+            }
+          })
+        ]
+      }
+    },
+    plugins: [react()],
+    ...(mode === "production" && {
+      esbuild: {
+        drop: ["console", "debugger"]
+      }
+    })
+  };
+});
