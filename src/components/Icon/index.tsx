025b31587978732b02c8bd084d9e4ebb1b506943
Nmgix <frostdraky@gmail.com>
2024-04-19 17:02:54 +0300

    URL fix try

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -6,6 +6,7 @@ export type IconProps = {
 };
 
 export const Icon: React.FC<IconProps> = ({ src, iconColor }) => {
+  // console.log(process.env.PUBLIC_URL);
   return (
     <i className='icon' style={{ backgroundColor: iconColor, WebkitMask: `url(${src}) 50% 50% / contain no-repeat`, WebkitMaskSize: "contain" }} />
   );
